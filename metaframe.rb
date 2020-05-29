69604affc146d44d5fbc218c7c24a17ba15c9253
runner <runner@Mac-1559.local>
2020-05-29 05:21:04 +0000

    metaframe v0.0.0a3

--- metaframe.rb
+++ metaframe.rb
@@ -1,9 +1,9 @@
 class Metaframe < Formula
   desc "A command line tool to run ETL jobs and view metadata"
   homepage "https://github.com/rsyi/metaframe"
-  url "https://github.com/rsyi/metaframe/releases/download/v0.0.0a2/metaframe-mac.tar.gz"
-  sha256 "d0510e6cae63a6cd2705ed2c7a473bd33b266d69f236cff78b878c725988c0b3"
-  version "v0.0.0a2"
+  url "https://github.com/rsyi/metaframe/releases/download/v0.0.0a3/metaframe-mac.tar.gz"
+  sha256 "716c80f7eb94aa5b53e053c79f9b6d301a6a41fade005b51ce772bfdf0d1c75e"
+  version "v0.0.0a3"
 
   depends_on "golang"
   depends_on "python"
