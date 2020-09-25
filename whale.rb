bdaa0908524a36360f8f67be326f342d55c1a4ab
Robert Yi <robert@ryi.me>
2020-09-25 08:14:16 -0700

    fix: match sha and version to newest version

--- whale.rb
+++ whale.rb
@@ -1,9 +1,9 @@
 class Whale < Formula
   desc "Data warehouse command line explorer"
   homepage "https://github.com/rsyi/whale"
-  url "https://github.com/rsyi/whale/releases/download/v0.0.0a32/whale-mac.tar.gz"
-  sha256 "3f33331207c6d83aa1fe22de3a630f800d65bc1b35ae310285e171d17bcffbbf"
-  version "v0.0.0b1"
+  url "https://github.com/rsyi/whale/releases/download/v0.0.0b0/whale-mac.tar.gz"
+  sha256 "12d448a3757b7f50017032df64f965a785957baa385e4838bb09a91d584980a6"
+  version "v0.0.0b0"
 
   depends_on "python"
 
