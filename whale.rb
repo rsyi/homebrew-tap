ed40aa9bb5086739562eddbcf1876a80d2343b5c
runner <runner@Mac-1601078494078.local>
2020-09-26 00:11:25 +0000

    whale v0.0.0b1

--- whale.rb
+++ whale.rb
@@ -1,9 +1,9 @@
 class Whale < Formula
   desc "Data warehouse command line explorer"
   homepage "https://github.com/rsyi/whale"
-  url "https://github.com/rsyi/whale/releases/download/v0.0.0b0/whale-mac.tar.gz"
-  sha256 "12d448a3757b7f50017032df64f965a785957baa385e4838bb09a91d584980a6"
-  version "v0.0.0b0"
+  url "https://github.com/rsyi/whale/releases/download/v0.0.0b1/whale-mac.tar.gz"
+  sha256 "9a09ed03a8ca0989547fb3749980cc71db6b633ffe8b37060a28934bfcf537d3"
+  version "v0.0.0b1"
 
   depends_on "python"
   depends_on "rust"
