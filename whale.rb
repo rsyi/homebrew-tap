b9ed9f96bdb05f4ec9c2fb045bed696ea7fa386b
Robert Yi <robert@ryi.me>
2020-09-30 14:28:41 -0700

    revert

--- whale.rb
+++ whale.rb
@@ -3,7 +3,7 @@ class Whale < Formula
   homepage "https://github.com/rsyi/whale"
   url "https://github.com/rsyi/whale/releases/download/v0.0.0b3/whale-mac.tar.gz"
   sha256 "c64b2ea47057f28319f6ad6729ad5ca478f71e546f6b229fbebfc9f07e1353be"
-  version "v0.0.0b3"
+  version "v0.0.0b0"
 
   depends_on "python"
   depends_on "rust"
