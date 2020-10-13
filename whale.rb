40f5b5e9ecb6b6614faf4e77c9f176bbd1d40fdb
Robert Yi <robert@ryi.me>
2020-10-13 09:22:12 -0700

    fix: broken brew

--- whale.rb
+++ whale.rb
@@ -1,17 +1,17 @@
 class Whale < Formula
   desc "Data warehouse command line explorer"
   homepage "https://github.com/rsyi/whale"
-  url "https://github.com/rsyi/whale/releases/download/v0.0.0b8/whale-mac.tar.gz"
-  sha256 "cf7b75b664229c26aaf3cb22c06a51ac0034b491887dd24b646e107723dfab3c"
-  version "v0.0.0b8"
+  url "https://github.com/rsyi/whale/releases/download/v0.0.0b7/whale-mac.tar.gz"
+  sha256 "374688e219374e765648c032d1bc336b6c470b00c27e95630df6af46cac77ebe"
+  version "v0.0.0b7"
 
-  depends_on "python"
+  depends_on "python@3.8"
   depends_on "rust"
 
   def install
-    system "make && make install && cp cli/target/release/whale cli/target/release/wh"
+    system "make"
+    system "cp cli/target/release/whale cli/target/release/wh"
     bin.install "cli/target/release/wh"
-    libexec.install "databuilder/build_script.py"
-    libexec.install "~/.whale/libexec/env"
+    libexec.install "build/*"
   end
 end
